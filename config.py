64ef158935ae723428886f57587ede7f152554ff
jemmyperez28 <jemmyperez28@gmail.com>
2020-10-14 19:24:38 -0500

    testmysql1
    Archivo de conexion y Tests de Prueba MySQL

---
+++ config.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+#clase de conexion
+class Conexion:
+
+    def __init__(self):
+        self.db = mysql.connector.connect(host="localhost", user="root", passwd="123456" , database="torneo",auth_plugin='mysql_native_password')
+
+    def conectar(self):
+        cur = self.db.cursor()
+        return cur
+
+    #def insertar(self):
+
+    def desconectar(self):
+        self.db.close()
+
+
+
--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,33 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from config import Conexion
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Prueba cambio4 en master branch'
+    return 'Test de Mostrar en Tablita en pythonanywhere'
 
+@app.route('/test')
+def test():
+    conecta = Conexion()
+    cur = conecta.conectar()
+    cur.execute("SELECT * FROM usuario")
+    data = cur.fetchone()
+    print(data)
+    return 'Test1' + str(data)
+
+@app.route('/test2')
+def test2():
+    conecta = Conexion()
+    cur = conecta.conectar()
+    cur.execute("SELECT * FROM usuario")
+    data = cur.fetchone()
+    print(data)
+    conecta.desconectar()
+    return 'Test1'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
